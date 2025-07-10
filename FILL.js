0e58471f2c5a2a494a0997b6440348902b090136
xiaothink <redminote13warnagold@gmail.com>
2025-07-11 01:09:44 +0700

    Update FILL.js

--- FILL.js
+++ FILL.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      7.42
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/xiaothink/JEKATE1/main/FILL.user.js
-// @downloadURL  https://raw.githubusercontent.com/xiaothink/JEKATE1/main/FILL.user.js
+// @updateURL    https://raw.githubusercontent.com/xiaothink/JEKATE1/main/FILL.js
+// @downloadURL  https://raw.githubusercontent.com/xiaothink/JEKATE1/main/FILL.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
